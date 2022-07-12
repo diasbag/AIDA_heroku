a683a43b99626208e19a80732b67e853f84fd2cd
dias.bagzat <diasbagzat1@gmail.com>
2022-07-12 16:15:16 +0600

    Auth Debug

--- src/main/java/com/hackathon/mentor/HackathonApp.java
+++ src/main/java/com/hackathon/mentor/HackathonApp.java
@@ -6,9 +6,9 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+//@EnableSwagger2
 @SpringBootApplication
 @SecurityScheme(name = "basicauth", scheme = "basic",
 		type = SecuritySchemeType.HTTP, in =
