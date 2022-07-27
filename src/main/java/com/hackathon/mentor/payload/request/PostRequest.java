9b14dacf7eccc0bef0c97a6fcbd676c59c7fd658
Saba <sabakurmashev@gmail.com>
2022-07-27 10:46:51 +0600

    posts update

--- src/main/java/com/hackathon/mentor/payload/request/PostRequest.java
+++ src/main/java/com/hackathon/mentor/payload/request/PostRequest.java
@@ -1,14 +1,21 @@
 package com.hackathon.mentor.payload.request;
 
 import com.hackathon.mentor.models.Image;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
-
+@Getter
+@Setter
+@ToString
 public class PostRequest {
+    @NotNull
+    private Long id;
 
 
     @NotNull
@@ -18,21 +25,4 @@ public class PostRequest {
     @NotNull
     private String article;
 
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-
-    public String getArticle() {
-        return article;
-    }
-
-    public void setArticle(String article) {
-        this.article = article;
-    }
 }
