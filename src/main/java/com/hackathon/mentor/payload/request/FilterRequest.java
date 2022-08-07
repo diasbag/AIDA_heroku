45427ed9de1e8db8d37cf0e12c0b2769cbff7d26
dias.bagzat <diasbagzat1@gmail.com>
2022-08-07 21:37:03 +0600

    *

--- src/main/java/com/hackathon/mentor/payload/request/FilterRequest.java
+++ src/main/java/com/hackathon/mentor/payload/request/FilterRequest.java
@@ -1,32 +1,10 @@
 package com.hackathon.mentor.payload.request;
 
+import lombok.Data;
+
+@Data
 public class FilterRequest {
     private String country;
     private String major;
-
     private String university;
-
-    public String getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(String university) {
-        this.university = university;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getMajor() {
-        return major;
-    }
-
-    public void setMajor(String major) {
-        this.major = major;
-    }
 }
--- src/main/java/com/hackathon/mentor/service/MentorService.java
+++ src/main/java/com/hackathon/mentor/service/MentorService.java
@@ -1,7 +1,9 @@
 package com.hackathon.mentor.service;
 
+import com.hackathon.mentor.models.Mentor;
 import com.hackathon.mentor.models.Skills;
 import com.hackathon.mentor.models.User;
+import com.hackathon.mentor.payload.request.FilterRequest;
 import com.hackathon.mentor.payload.request.SignupUpdateMentorRequest;
 import org.springframework.http.ResponseEntity;
 
@@ -12,7 +14,8 @@ public interface MentorService {
     ResponseEntity<?> getUserProfile(User user);
 
     ResponseEntity<?> getMentors(Integer page);
-//    ResponseEntity<?> filtration(String country, String major, String university);
+
+    ResponseEntity<?> filtration(FilterRequest request, Integer page);
 
     ResponseEntity<?> getMentorById(Long id);
 
@@ -21,6 +24,8 @@ public interface MentorService {
     ResponseEntity<?> updateMentor(String email, SignupUpdateMentorRequest updateMentorRequest);
 
     ResponseEntity<?> getMySubscribers(String email);
+
+    ResponseEntity<?> getMentorByUniversity(String name);
 
     ResponseEntity<?> confirm(Long id, String email);
 
